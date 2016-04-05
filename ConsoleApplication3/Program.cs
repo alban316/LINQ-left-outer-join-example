954bc2f1597b68fc0b9859c09e7e12d119e56937
Alban316 <jason.brugger@gmail.com>
2016-04-05 09:23:22 -0400

    initial commit

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] iLeft = { 1, 2, 3 };
+            int[] iRight = { 3, 4, 5 };
+
+            List<int> left = iLeft.ToList();
+            List<int> right = iRight.ToList();
+
+            var q = (
+                from l in left
+                join r in right on l equals r into leftright
+                from lr in leftright.DefaultIfEmpty()
+                where lr == 0
+                select l).ToList();
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+/*
+
+
+
+*/
